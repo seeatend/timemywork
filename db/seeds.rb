c9affab66ef2010a4d9bee6fb3c33ecc2538799b
Abubakr Abd El Hafiz <abubakr35@hotmail.com>
2018-04-11 02:06:46 +0200

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create!([
-  { name: 'Product 001' },
-  { name: 'Product 002' },
-  { name: 'Product 003' },
-  { name: 'Product 004' }
-])
-Customer.create!([
-  { name: 'Customer 001' },
-  { name: 'Customer 002' },
-  { name: 'Customer 003' },
-  { name: 'Customer 004' }
-])
-User.create!([
-  { name: 'User 001' },
-  { name: 'User 002' },
-  { name: 'User 003' },
-  { name: 'User 004' }
-])
-Order.create!([
-  { reference: 'ORD001', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
-  { reference: 'ORD002', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
-  { reference: 'ORD003', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
-  { reference: 'ORD004', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
-  { reference: 'ORD005', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours }
-])
+# Product.create!([
+#   { name: 'Product 001' },
+#   { name: 'Product 002' },
+#   { name: 'Product 003' },
+#   { name: 'Product 004' }
+# ])
+# Customer.create!([
+#   { name: 'Customer 001' },
+#   { name: 'Customer 002' },
+#   { name: 'Customer 003' },
+#   { name: 'Customer 004' }
+# ])
+# User.create!([
+#   { name: 'User 001' },
+#   { name: 'User 002' },
+#   { name: 'User 003' },
+#   { name: 'User 004' }
+# ])
+# Order.create!([
+#   { reference: 'ORD001', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
+#   { reference: 'ORD002', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
+#   { reference: 'ORD003', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
+#   { reference: 'ORD004', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours },
+#   { reference: 'ORD005', user_id: 1, starttime: Time.now - 10.hours, endtime: Time.now - 5.hours }
+# ])
+
+# name, :email, :time_rate, :day_rate, :night_rate, :time_cost, :day_cost, :night_cost, :fixed_rate
+m = Member.new
+m.name = 'Admin'
+m.email = 'admin@domain.com'
+m.time_rate = 10
+m.day_rate = 15
+m.night_rate = 20
+m.time_cost = 25
+m.day_cost = 25
+m.night_cost = 25
+m.fixed_rate = 30
+
+m.save
+
+admin = Admin.new
+admin.email = 'admin@domain.com'
+admin.password = '123456'
+admin.confirm_password = '123456'
+admin.member_id = m.id
