b1f5fe5a4e072f00ea917b4e0edc167619f60493
Abubakr Abd El Hafiz <abubakr35@hotmail.com>
2018-04-13 00:23:50 +0200

    orders views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
 
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglifier
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
