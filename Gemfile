81f386c2299fa6b0ed8e55a5f575f388bfb4a4fa
Abubakr Abd El Hafiz <abubakr35@hotmail.com>
2018-04-14 16:06:04 +0200

    fix login layout

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+# gem 'bcrypt', platforms: :ruby
 gem 'sass-rails'
 gem 'uglifier'
 gem 'coffee-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-  # layout :layout
+   layout :layout
 
     private
 
-    def layout
-      # only turn it off for login pages:
-      is_a?(Devise::SessionsController) ? false : "application"
-      # or turn layout off for every devise controller:
-      devise_controller? && "application"
-    end
+   def layout
+     devise_controller? ? false : "application"
+
+   end
 end
